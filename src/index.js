783cb1b37da6090c075fe67513567165980ab75b
Martin-Ocean <gong62@purdue.edu>
2017-11-29 00:17:24 -0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class DigitalClock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    
+    this.timerID = setInterval (
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date:new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> {this.state.date.toLocaleTimeString()} </h1>
+      </div>
+    );
+  }
+
+
+}
+
+ReactDOM.render(<DigitalClock />, document.getElementById('root'));
